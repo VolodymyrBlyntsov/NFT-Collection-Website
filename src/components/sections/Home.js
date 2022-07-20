45d98879095ebe3e1300291babe92da7e04a6f51
Volodymyr <volodymyr.blyntsov@gmail.com>
2022-07-20 16:40:44 +0300

    NFT Page
    * Starting "home page" layout, implemented part of the "welcome" (text), for textwriter used the appropriate library.

--- src/components/sections/Home.js
+++ src/components/sections/Home.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import TypeWriterText from '../TypeWriterText';
+
+const Section = styled.section`
+  min-height: ${props => `calc(100vh - ${props.theme.navHeight})` };
+  width: 100vm;
+  position: relative;
+  background-color: ${props => props.theme.body};
+`
+
+const Container = styled.div`
+  width: 75%;
+  min-height: 80vh;
+  margin: 0 auto;
+  background-color: lightblue;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Box = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Section>
+      <Container>
+        <Box> <TypeWriterText /> </Box>
+        <Box>video</Box>
+      </Container>
+    </Section>
   )
 }
 
