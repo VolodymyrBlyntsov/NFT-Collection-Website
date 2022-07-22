9be2a2fa5e90915264cfe61c261436f6ace07913
Volodymyr <volodymyr.blyntsov@gmail.com>
2022-07-22 13:55:36 +0300

    NFT Page
    * The stage-shower has been redesigned.
    
    * Roadmap sidebars were realized.
    
    (gsap was used extensively while developing RoadMap).

--- src/components/sections/Roadmap.js
+++ src/components/sections/Roadmap.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import React, { useLayoutEffect, useRef } from 'react';
 import styled from 'styled-components';
 import DrawSvg from '../DrawSvg';
 
@@ -6,7 +8,7 @@ const Section = styled.section`
   min-height: 100vh;
   width: 100vw;
   background-color: ${props => props.theme.body};
-  position relative;
+  position: relative;
 `
 
 const Title = styled.h1`
@@ -29,7 +31,7 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  position: relative; 
+  position: relative;
 `
 
 const SvgContainer = styled.div`
@@ -38,16 +40,136 @@ const SvgContainer = styled.div`
   align-items: center;
 `
 
+const Items = styled.ul`
+  list-style: none;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  &>*:nth-of-type(2n + 1) {
+    justify-content: start;
+    div {
+      border-radius: 50px 0 50px 0;
+      text-align: right;
+    }
+    p {
+      border-radius: 40px 0 40px 0;
+    }
+  }
+  &>*:nth-of-type(2n) {
+    justify-content: end;
+    div {
+      border-radius: 0 50px 0 50px;
+      text-align: left;
+    }
+    p {
+      border-radius: 0 40px 0 40px;
+    }
+  }
+`
+
+const Item = styled.li`
+  width: 100%;
+  height: 100%;
+  display: flex;
+`
+
+const ItemContainer = styled.div`
+  width: 40%;
+  height: fit-content;
+  padding: 1rem;
+  border: 3px solid ${props => props.theme.text};
+`
+
+const Box = styled.p`
+  height: fit-content;
+  background-color: ${props => props.theme.carouselColor};
+  color: ${props => props.theme.text};
+  padding: 1rem;
+  position: relative;
+  border: 1px solid ${props => props.theme.text};
+
+`
+
+const SubTitle = styled.span`
+  display: block;
+  font-size: ${props => props.theme.fontxl};
+  text-transform: capitalize;
+  color: ${props => props.theme.text};
+`
+const Text = styled.span`
+  display: block;
+  font-size: ${props => props.theme.fontsm};
+  color: ${props => props.theme.text};
+  font-weight: 400;
+  margin: 0.5rem 0;
+`
+
+const RoadMapItem = ({title, subtext, addToRefs}) => {
+
+  return (
+    <Item ref={addToRefs}>
+      <ItemContainer>
+        <Box>
+          <SubTitle> {title} </SubTitle>
+          <Text> {subtext} </Text>
+        </Box>
+      </ItemContainer>
+    </Item>
+  )
+}
+
 const Roadmap = () => {
+
+  const revealRefs = useRef([]);
+  revealRefs.current = [];
+  gsap.registerPlugin(ScrollTrigger);
+  
+  const addToRefs = (el) => {
+    if (el && !revealRefs.current.includes(el)) {
+      revealRefs.current.push(el);
+    }
+  }
+
+  useLayoutEffect(() => {
+    let t1 = gsap.timeline();
+    revealRefs.current.forEach((el, index) => {
+      t1.fromTo(
+        el.childNodes[0],
+        {
+          y: '0'
+        }, {
+          y: '-30%',
+          scrollTrigger:{
+            id: `section-${index + 1}`,
+            trigger: el,
+            start: 'top center+=200px',
+            end: 'bottom center',
+            scrub: true,
+          }
+        }
+      )
+    })
+  }, [])
+
   return (
     <Section>
-      <Title>
-        Roadmap
-      </Title>
+      <Title>Roadmap</Title>
       <Container>
         <SvgContainer>
           <DrawSvg />
         </SvgContainer>
+        <Items>
+          <Item>&nbsp;</Item> {/* empty item */}
+          <RoadMapItem addToRefs={addToRefs} title="Grand Opening" subtext="Get ready for the world launch of a new, unique, unusual NFT collection 'Tadpoles Club' in the coming months!" />
+          <RoadMapItem addToRefs={addToRefs} title="Great Benefits" subtext="Pre-order the collection at a starting price, as well as get a free NFT on our discord server." />
+          <RoadMapItem addToRefs={addToRefs} title="Early Access" subtext="Be active in our social networks, as the same in our discord server, to get into the white-list and get a chance to receive the nft one of the first." />
+          <RoadMapItem addToRefs={addToRefs} title="New Merch" subtext="Upcoming plans are to launch a collection of 3D figures with the already beloved Tadpoles! Also expect cups with unique NFT avatar and uid number for free  ." />
+          <RoadMapItem addToRefs={addToRefs} title="Holders Ranking" subtext="As the longer you have the Tadpoles, the more expensive they become! Turn on the notifications and don't forget to snag an NFT to top the owner-list." />
+        </Items>
       </Container>
     </Section>
   )
