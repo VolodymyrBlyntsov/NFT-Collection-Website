c1ad0658d2a0627c25317de6a16f7c6ab88f4c72
Volodymyr <volodymyr.blyntsov@gmail.com>
2022-07-23 11:49:42 +0300

    NFT Page
    * Added a block of answers about the project (FAQ).

--- src/components/sections/Faq.js
+++ src/components/sections/Faq.js
@@ -1,8 +1,69 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import Accordion from '../Accordion';
+
+const Section = styled.section`
+  min-height: 100vh;
+  width: 100vw;
+  background-color: ${props => props.theme.text};
+  color: ${props => props.theme.body};
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+const Title = styled.h1`
+  font-size: ${props => props.theme.fontxxl};
+  text-transform: uppercase;
+  color: ${props => props.theme.body};
+  margin: 1rem auto;
+  border-bottom: 2px solid ${props => props.theme.body};
+  width: fit-content;
+`
+
+const Container = styled.div`
+  width: 75%;
+  margin: 2rem auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const Box = styled.div`
+  width: 45%;
+`
 
 const Faq = () => {
   return (
-    <div>Faq</div>
+    <Section>
+      <Title>faq</Title>
+      <Container>
+        <Box>
+          <Accordion title='where can i view my nfts?'>
+            Once minted or bought simply connect to your OpenSea account to view your NFTs.
+          </Accordion>
+          <Accordion title='what is the metaverse?'>
+            A metaverse is a network of 3D virtual worlds focused on social connection. In futurism and science fiction, it is often described as a hypothetical iteration of the Internet as a single, universal virtual world that is facilitated by the use of virtual and augmented reality headsets.
+          </Accordion>
+          <Accordion title='why do we need royalties?'>
+            The amount of royalties was fixed at 5% to finance the Tadlopes Club's projects. We have the ambition to organize multiple events around the world in order to strengthen the community and build a network of entrepreneurs and investors with the same mindset and common interests.
+          </Accordion>
+        </Box>
+        <Box>
+          <Accordion title='how can i use my nft?'>
+            You will be able to use your NFT as an avatar in the Metaverse and our future video game. Holding also means that you are part of an exclusive network of investors and entrepreneurs.
+          </Accordion>
+          <Accordion title='what are the tadpoles?'>
+            Tadpoles is a private collection of NFTs — unique digital collectibles. The Tadpoles are stored as ERC-721 tokens on the Ethereum blockchain and hosted on IPFS.
+          </Accordion>
+          <Accordion title='what is the fusion process?'>
+            The fusion process allows you to dynamically render about 10 new, unique NFTs per day, independent of the software.
+          </Accordion>
+        </Box>
+      </Container>
+    </Section>
   )
 }
 
