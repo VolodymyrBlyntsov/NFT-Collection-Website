754cd39301007db9676ca9f3efd055778ead83cc
Volodymyr <volodymyr.blyntsov@gmail.com>
2022-07-24 21:04:46 +0300

    NFT Collection Website
    * Fixed bug with a non-working buttons (all buttons are clickable, except the one in the footer, part of the banner. It specifically does not lead anywhere and does not represent anything underneath).

--- src/components/utils/Button.js
+++ src/components/utils/Button.js
@@ -39,11 +39,9 @@ const Btn = styled.button`
 
 const Button = ({text, link}) => {
   return (
-    <Btn>
-        <a href={link} aria-label={text} target="_blank">
-            {text}
+        <a href={link} aria-label={text} target="_blank" rel="noreferrer">
+            <Btn>{text}</Btn>
         </a>
-    </Btn>
   )
 }
 
