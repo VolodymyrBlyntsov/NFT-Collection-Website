a3d735ebdcc44ac01d98be84207649b54bae49a4
Volodymyr <volodymyr.blyntsov@gmail.com>
2022-07-24 14:53:44 +0300

    NFT Collection Website
    * Added hamburger menu to navigation (when screen size <1024).

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Logo from './utils/Logo';
 import Button from './utils/Button';
@@ -14,12 +14,43 @@ const NavBar = styled.nav`
   width: 85%;
   height: ${props => props.theme.navHeight};
   margin: 0 auto;
+
+  .mobile {
+    display: none;
+  }
+
+  @media (max-width: 64em) {
+    .desktop {
+      display: none;
+    }
+    .mobile {
+      display: inline-block;
+    }
+  }
 `
 const Menu = styled.ul`
   display: flex;
   justify-content: space-between;
   align-item: center;
   list-style: none; 
+  
+  @media (max-width: 64em) {
+    position: fixed;
+    top: ${props => props.theme.navHeight};
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100vw;
+    height: ${props => `calc(100vh - ${props.theme.navHeight})`};
+    z-index: 50;
+    background-color: ${props => `rgba(${props.theme.bodyRgba}, 0.85)`};
+    transform: ${props => props.click ? 'translateY(0)' : 'translateY(100%)'};
+    backdrop-filter: blur(2px);
+    transition: all 0.3s ease;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 `
 
 const MenuItem = styled.li`
@@ -27,8 +58,8 @@ const MenuItem = styled.li`
   color: ${props => props.theme.text};
   cursor: pointer;
 
-  &:after {
-    content: '';
+  &::after {
+    content: ' ';
     display: block;
     width: 0%;
     height: 2px;
@@ -39,31 +70,94 @@ const MenuItem = styled.li`
   &:hover::after {
     width: 100%;
   }
+
+  @media (max-width: 64em) {
+    margin: 1rem 0;
+    
+    &::after {
+      display: none; 
+    }
+  }
 `
 
-const scrollTo = (id) => {
+const HamburgerMenu = styled.span`
+  width: ${props => props.click ? '2rem' : '1.5rem'};
+  height: 2px;
+  background: ${props => props.theme.text};
+
+  position: absolute;
+  top: 2rem;
+  left: 50%;
+  transform: ${props => props.click ? 'translateX(-50%) rotate(90deg)' : 'translateX(-50%) rotate(0)'};
+
+  display: none;
+  justify-content: center;
+  align-items: center;
+
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  @media (max-width: 64em) {
+    display: flex;
+  }
+
+  &::after, &::before {
+    content: ' ';
+    width: ${props => props.click ? '1rem' : '1.5rem'};
+    height: 2px;
+    right: ${props => props.click ? '-2px' : '0'};
+    background: ${props => props.theme.text};
+    position: absolute;
+    transition: all 0.3s ease;
+  }
+
+  &::after {
+    top: ${props => props.click ? '0.3rem' : '0.5rem'};
+    transform: ${props => props.click ? 'rotate(-40deg)' : 'rotate(0)'}
+  }
+
+  &::before {
+    bottom: ${props => props.click ? '0.3rem' : '0.5rem'};
+    transform: ${props => props.click ? 'rotate(40deg)' : 'rotate(0)'}
+  }
+`
+
+const Navigation = () => {
+  const [click, setClick] = useState(false)
+  
+  const scrollTo = (id) => {
   let element = document.getElementById(id)
   element.scrollIntoView({
     behavior: 'smooth',
     block: 'start',
     inline: 'nearest'
   })
+  setClick(!click);
 }
 
-const Navigation = () => {
   return (
     <Section id="navigation">
       <NavBar>
         <Logo />
-        <Menu>
+        <HamburgerMenu click={click} onClick={() => setClick(!click)}>
+          &nbsp;
+        </HamburgerMenu>
+        <Menu click={click}>
           <MenuItem onClick={() => scrollTo('home')}>Home</MenuItem>
           <MenuItem onClick={() => scrollTo('about')}>About</MenuItem>
           <MenuItem onClick={() => scrollTo('roadmap')}>Roadmap</MenuItem>
           <MenuItem onClick={() => scrollTo('showcase')}>Showcase</MenuItem>
           <MenuItem onClick={() => scrollTo('team')}>Team</MenuItem>
           <MenuItem onClick={() => scrollTo('faq')}>Faq</MenuItem>
+          <MenuItem>
+            <div className="mobile">
+              <Button text='Connect Wallet' link='https://metamask.io/' />
+            </div>
+          </MenuItem>
         </Menu>
-        <Button text='Connect Wallet' link='https://metamask.io/' />
+        <div className="desktop">
+          <Button text='Connect Wallet' link='https://metamask.io/' />
+        </div>
       </NavBar>
     </Section>
   )
